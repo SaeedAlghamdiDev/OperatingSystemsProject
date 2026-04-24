0a570b3206efb6bae0ab8f83f69072e936beca72
DarkOne100 <crystalsage_@hotmail.com>
2026-04-24 14:32:22 +0300

    Component Design Change
    Switch from multi-component to single component for handling data input

--- OperatingSystemsProject/src/CPUSchedulingAlgorithmInput.jsx
+++ OperatingSystemsProject/src/CPUSchedulingAlgorithmInput.jsx
@@ -6,6 +6,58 @@ function CPUSchedulingAlgorithmInput(){
     
 
     const [numberOfTasks, setNumberOfTasks] = useState(0);
+    const [listOfTasks, setListOfTasks] = useState([]);
+    const [arrivalTime, setArrivalTime] = useState(0);
+    const [burstTime, setBurstTime] = useState(0);
+    const [quantumTime, setQuantumTime] = useState(0);
+
+    let quantum = true;
+
+
+    const addTask = () => {
+
+        let newTask = null;
+        quantum?  newTask = {TaskArrivalTime: arrivalTime,
+                                  TaskBurstTime:burstTime,
+                                  TaskQuantumTime:quantumTime}
+                                  :
+                  newTask = {TaskArrivalTime: arrivalTime,
+                                  TaskBurstTime:burstTime};
+
+        setListOfTasks(l => [...l, newTask]);
+
+        
+
+
+        
+    }
+
+
+    const removeTask = (index) =>{
+
+        setListOfTasks(l => l.filter((_, i) => i !== index));
+    }
+    const handleArrivalTimeChange = (event) => {
+            
+            setArrivalTime(parseInt(event.target.value));
+            
+    }
+    const handleBurstTimeChange = (event) => {
+            
+            setBurstTime(parseInt(event.target.value));
+            
+    }
+    const handleQuantumTimeChange = (event) => {
+            
+            setQuantumTime(parseInt(event.target.value));
+            
+    }
+
+    const handleListOfTasks= () => {
+
+        setListOfTasks("");
+
+    }
 
     const handleTasksChange = (event) => {
         
@@ -20,16 +72,12 @@ function CPUSchedulingAlgorithmInput(){
     }
 
     //Building an array of <TaskDataInputs>
-    const items = [];
-    for(let i = 1; i < numberOfTasks + 1; i++){
-        items.push(<TaskDataInput name = {i} quantum = {true}/>)
-    }
+    // const items = [];
+    // for(let i = 1; i < numberOfTasks + 1; i++){
+    //     items.push(<TaskDataInput name = {i} quantum = {true}/>)
+    // }
 
-    const collectData = (event) => {
-
-        
-
-    }
+    
     
 
     
@@ -37,18 +85,27 @@ function CPUSchedulingAlgorithmInput(){
     return(<>
 
         <div>
-        <p>How many tasks will the CPU handle? 
-             <input type="number" value={numberOfTasks} onChange={handleTasksChange}></input>
-        </p>
-        </div>
-
-        <p>
+     <input type="number" value={arrivalTime} onChange={handleArrivalTimeChange} ></input>
+                       <input type="number" value={burstTime} onChange={handleBurstTimeChange} ></input>
+                       {quantum == true? <input type="number" value={quantumTime} onChange={handleQuantumTimeChange} ></input>: null}
+    
+    <button onClick={addTask}>Add Task</button>
+    </div>
         
+        <div>Added Tasks: <br/>
+        <uo>
+            {listOfTasks.map((listOfTasks, index)=>
+                 <li key={index} onClick={() => removeTask(index)}>
+                     Task: {index + 1} Arrival Time: {listOfTasks.TaskArrivalTime}s Burst Time: {listOfTasks.TaskBurstTime}s  </li>)}
+        </uo>
 
-        {items}
+    </div>
 
-        </p>
-        
+
+    <div>
+        {/* <button onClick={CalculateRequest}>Calculate</button> */}
+    </div>
+
         
 
     </>)
